47264152d8c4c469037f801333105dbdab7741c5
afuawonders <afuawonders@gmail.com>
2023-07-13 23:37:14 +0000

    python import module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b))
+    print("{} + {} = {}".format(a, b, addition(a, b)))
+    print("{} - {} = {}".format(a, b, subtraction(a, b)))
+    print("{} * {} = {}".format(a, b, multiplication(a, b)))
+    print("{} / {} = {}".format(a, b, division(a, b))
