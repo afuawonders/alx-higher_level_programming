f1d18ddf8e7be71ae7421c21828fb84f2087f596
afuawonders <afuawonders@gmail.com>
2023-07-20 00:00:31 +0000

    python more data structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <Python.h>
+#include <python.h>
 
 /**
  * print_python_bytes - Prints bytes information
+ *
  * @p: Python Object
  * Return: no return
  */
 void print_python_bytes(PyObject *p)
 {
 	char *string;
-	long int size, j, limit;
+	long int size, i, limit;
 
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
@@ -31,23 +32,24 @@ void print_python_bytes(PyObject *p)
 
 	printf("  first %ld bytes:", limit);
 
-	for (j = 0; j < limit; j++)
-		if (string[j] >= 0)
-			printf(" %02x", string[j]);
+	for (i = 0; i < limit; i++)
+		if (string[i] >= 0)
+			printf(" %02x", string[i]);
 		else
-			printf(" %02x", 256 + string[j]);
+			printf(" %02x", 256 + string[i]);
 
 	printf("\n");
 }
 
 /**
  * print_python_list - Prints list information
+ *
  * @p: Python Object
  * Return: no return
  */
 void print_python_list(PyObject *p)
 {
-	long int size, j;
+	long int size, i;
 	PyListObject *list;
 	PyObject *obj;
 
@@ -58,10 +60,10 @@ void print_python_list(PyObject *p)
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
-		obj = ((PyListObject *)p)->ob_item[j];
-		printf("Element %ld: %s\n", j, ((obj)->ob_type)->tp_name);
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 	}
