8dfd2321f079a5fb18231a26e8da29b5249c8812
afuawonders <afuawonders@gmail.com>
2023-08-08 00:13:04 +0000

    Python driven test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
+import unittest
+max_integer = _import_('6-max_integer').max_integer
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
 
+    def test_positive_numbers(self):
+        self.assertEqual(max_integer([1, 5, 3, 9, 2]), 9)
+    
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-5, -2, -10, -1]), -1)
+    
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-5, 0, 15, -1]), 15)
+    
+    def test_single_element(self):
+        self.assertEqual(max_integer([7]), 7)
+    
+    def test_duplicate_max(self):
+        self.assertEqual(max_integer([2, 2, 2]), 2)
 
-def max_integer(list=[]):
-    """Function to  return the max integer in a list of integers
-        Return  None if list is empty
-    """
-    if len(list) == 0:
-        return (None)
-    answer = list[0]
-    index = 1
-    while index < len(list):
-        if list[index] > answer:
-            answer = list[index]
-        index += 1
-    return (answer)
+if _name_ == '_main_':
+    unittest.main()
