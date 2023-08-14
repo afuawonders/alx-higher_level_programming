4648010f98d2514725fd3e3e79fd9009540c6b56
afuawonders <afuawonders@gmail.com>
2023-08-14 22:30:43 +0000

    python inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
+"""Defines a class and inherited class-checking function."""
 
-"""Return instance of a specific class."""
 
-
-def is_same_class(obj, a_class):
-
-    """Check if an object is an instance of a given class.
+def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance or inherited instance of a class.
 
     Args:
-
         obj (any): The object to check.
-
-        a_class (type): The class to compare the type of obj to.
-
+        a_class (type): The class to match the type of obj to.
     Returns:
-
-        Boolean of an instance of a_class.
-
+        If obj is an instance or inherited instance of a_class - True.
+        Otherwise - False.
     """
-
-    if type(obj) == a_class:
-
+    if isinstance(obj, a_class):
         return True
-
     return False
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,16 +1,10 @@
-
 #!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
 
-"""Defines class BaseGeometry."""
 
 class BaseGeometry:
-
-    """Class body."""
-
-
+    """Represent base geometry."""
 
     def area(self):
-
         """Not implemented."""
-
         raise Exception("area() is not implemented")
