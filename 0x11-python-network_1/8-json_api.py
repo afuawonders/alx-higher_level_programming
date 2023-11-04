c99c9d049c00d414fb2a83876049f42e0b931e30
afuawonders <afuawonders@gmail.com>
2023-11-04 00:34:46 +0000

    python network 1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
-"""A Python script that takes in a letter and sends a POST request 
-to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+"""Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter"""
 
+from sys import argv
+from requests import post
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    letter = argv[1] if len(argv) >= 2 else ""
+    response = post(url, data={'q': letter})
+
+    content_type = response.headers['content-type']
+
+    if content_type == 'application/json':
+        json_data = response.json()
+        user_id = json_data.get('id')
+        user_name = json_data.get('name')
+        if user_id and user_name:
+            print("[{}] {}".format(user_id, user_name))
+        else:
+            print('No result')
+    else:
+        print('Not a valid JSON')
