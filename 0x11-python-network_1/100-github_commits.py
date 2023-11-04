cb395d38a37a21370ec03c567a2f8ab1d87cb661
afuawonders <afuawonders@gmail.com>
2023-11-04 02:09:45 +0000

    python network 1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     commits = m.json()
     try:
         for i in range(10):
-            print(f"{commits[i]['sha']}:
-                    {commits[i]['commit']['author']['name']}")
+            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
     except IndexError:
         pass
