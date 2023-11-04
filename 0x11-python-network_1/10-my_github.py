0d57e56f436c7061328b093f27c2f0f1d070da9c
afuawonders <afuawonders@gmail.com>
2023-11-04 01:10:20 +0000

    python network 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-"""A Python script that takes your GitHub credentials(username and password) 
+"""A Python script that takes your GitHub credentials(username and password)
 and uses the GitHub API to display your id"""
 
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    data_result = requests.get(url, auth=(user, passwd))
+
+    try:
+        data_json = data_result.json()
+        print(data_json["id"])
+    except Exception:
+        print("None")
